73dcefefe44fe54b9b4702515cf23dc9e8641732
Seb <spelletier1996@gmail.com>
2019-02-06 19:51:51 -0500

    funtion

--- Software/Functions/servo_Actuate.c
+++ Software/Functions/servo_Actuate.c
@@ -3,7 +3,7 @@
 
 Void level_Corner(servo current_Servo, float correction) { //Function to control the servo rotation and travel of the bed leveling screw
 
-      static float screw_Pitch = 0.5, lead_Distance, num_Threads = 1, correction_Angle; //Variables required to calculate the distance traveled by the bed
+      static float screw_Pitch = 0.5 , lead_Distance, num_Threads = 1, correction_Angle; //Variables required to calculate the distance traveled by the bed
 
       lead_Distance = screw_Pitch * num_Threads; //Lead distance of screw calculation
 
